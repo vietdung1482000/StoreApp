99b995dee596aa9232d01fab1500bbe09fcb657c
unknown <vietdung>
2023-01-10 08:08:55 +0700

    update

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
 import Helmet from '../components/Helmet/Helmet'
 import { Container, Row, Col, Form, FormGroup } from 'reactstrap'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import '../style/login.css'
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from '../firebase.config'
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage'
+import { auth, storage, db } from '../firebase.config'
+import { setDoc, doc } from 'firebase/firestore'
+import { toast } from 'react-toastify'
 
 const SignUp = () => {
   const [username, setUsername] = useState("")
@@ -12,17 +15,47 @@ const SignUp = () => {
   const [password, setPassword] = useState("")
   const [file, setFile] = useState(null)
   const [loading, setLoading] = useState(null)
+  const navigate = useNavigate()
 
   const signup = async (e) => {
     e.preventDefault()
     setLoading(true)
     try {
       const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
       const user = userCredential.user
-      console.log("user", user);
+      const storageRef = ref(storage, `images/${Date.now() + username}`)
+      const uploadTask = uploadBytesResumable(storageRef, file)
+
+      uploadTask.on((error) => {
+        toast.error(error.message)
+      }, () => {
+        getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+
+          // update user profile
+          await updateProfile(user, {
+            displayName: username,
+            photoURL: downloadURL
+          })
+
+          // store user data in firestore database
+          await setDoc(doc(db, 'users', user.uid), {
+            uid: user.uid,
+            displayName: username,
+            email,
+            photoURL: downloadURL
+          })
+
+        })
+      })
+
+      setLoading(false)
+      toast.success('Account created')
+      navigate('/login')
 
     } catch (error) {
-
+      setLoading(false)
+      toast.error('something went wrong!')
     }
   }
 
@@ -31,27 +64,31 @@ const SignUp = () => {
       <section>
         <Container>
           <Row>
-            <Col lg='6' className='m-auto text-center'>
-              <h3 className='fw-bold mb-4'>Sign Up</h3>
+            {
+              loading
+                ? <Col lg='12' className='text-center'><h5 className='fw-bold'>Loading............</h5></Col>
+                : <Col lg='6' className='m-auto text-center'>
+                  <h3 className='fw-bold mb-4'>Sign Up</h3>
 
-              <Form className='auth_form' onSubmit={signup}>
-                <FormGroup className='form-group'>
-                  <input type="text" placeholder='UserName' value={username} onChange={e => setUsername(e.target.value)} />
-                </FormGroup>
-                <FormGroup className='form-group'>
-                  <input type="email" placeholder='Enter your email' value={email} onChange={e => setEmail(e.target.value)} />
-                </FormGroup>
-                <FormGroup className='form-group'>
-                  <input type="password" placeholder='Enter your password' value={password} onChange={e => setPassword(e.target.value)} />
-                </FormGroup>
-                <FormGroup className='form-group'>
-                  <input type="file" onChange={e => setFile(e.target.files[0])} />
-                </FormGroup>
+                  <Form className='auth_form' onSubmit={signup}>
+                    <FormGroup className='form-group'>
+                      <input type="text" placeholder='UserName' value={username} onChange={e => setUsername(e.target.value)} />
+                    </FormGroup>
+                    <FormGroup className='form-group'>
+                      <input type="email" placeholder='Enter your email' value={email} onChange={e => setEmail(e.target.value)} />
+                    </FormGroup>
+                    <FormGroup className='form-group'>
+                      <input type="password" placeholder='Enter your password' value={password} onChange={e => setPassword(e.target.value)} />
+                    </FormGroup>
+                    <FormGroup className='form-group'>
+                      <input type="file" onChange={e => setFile(e.target.files[0])} />
+                    </FormGroup>
 
-                <button type='submit' className='auth_btn'>Create An Account</button>
-                <p>Already have an account? <Link to='/login'>Login</Link></p>
-              </Form>
-            </Col>
+                    <button type='submit' className='auth_btn'>Create An Account</button>
+                    <p>Already have an account? <Link to='/login'>Login</Link></p>
+                  </Form>
+                </Col>
+            }
           </Row>
         </Container>
       </section>
